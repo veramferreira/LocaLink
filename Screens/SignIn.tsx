9e43265c343161a461c2d611ed12ec25166f0cf7
Vera Ferreira <verammferreira@gmail.com>
2023-06-12 15:01:42 +0100

    add sign-in page

--- Screens/SignIn.tsx
+++ Screens/SignIn.tsx
@@ -1,21 +1,23 @@
 import { Text, View, StyleSheet, Button } from "react-native";
+// import Google from "react-native-google-button";
+import { GoogleSignin, GoogleSigninButton } from "react-native-google-signin";
+import { auth } from "../config/firebase";
+import { GoogleAuthProvider, signInWithRedirect, getAuth } from "firebase/auth";
 
-export default function SignIn ({navigation}: any) {
-    return (
-        <Button
-      title="Sign in"
-      onPress={() =>
-        navigation.navigate('SignUp', {name: 'Jane'})
-      }
-    />
-    )
+const googleSignIn = () => {
+  const provider = new GoogleAuthProvider();
+  signInWithRedirect(auth, provider);
+};
+
+export default function SignIn({ navigation }: any) {
+  return <GoogleSigninButton onPress={googleSignIn} />;
 }
 
 const styles = StyleSheet.create({
-    sign_in: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  sign_in: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- config/firebase.js
+++ config/firebase.js
@@ -2,6 +2,9 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { Constants } from "expo-constants";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,3 +23,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
