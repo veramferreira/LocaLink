ebdca41ad3d16c2cfb260e70edb4e843a5c138eb
Vera Ferreira <verammferreira@gmail.com>
2023-06-12 12:08:58 +0100

    add logo to assets folder

--- app.config.js
+++ app.config.js
@@ -25,13 +25,14 @@ export default {
     },
     web: {
       favicon: "./assets/favicon.png",
-    },extra:{
+    },
+    extra: {
       apiKey: process.env.API_KEY,
       authDomain: process.env.AUTH_DOMAIN,
       projectId: process.env.PROJECT_ID,
       storageBucket: process.env.STORAGE_BUCKET,
       messagingSenderId: process.env.MESSAGING_SENDER_ID,
       appId: process.env.APP_ID,
-    },,
+    },
   },
 };
