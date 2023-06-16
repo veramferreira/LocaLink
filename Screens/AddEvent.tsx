e0deed5543be4d5998242ea0953adfe51a5afd60
Alina Khvishchuk <alina.khvishchuk@gmail.com>
2023-06-16 11:44:09 +0100

    Add a form to AddEvent component

--- Screens/AddEvent.tsx
+++ Screens/AddEvent.tsx
@@ -1,9 +1,188 @@
-import { Text, View, StyleSheet, Button } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Alert,
+} from "react-native";
+import { Formik, FormikProps } from "formik";
+import { db } from "../config/firebase";
+import { addDoc, collection } from "firebase/firestore";
+import * as yup from "yup";
+import { useNavigation } from "@react-navigation/native";
 
-export default function AddEvent({ navigation }: any) {
-  return (
-    <>
-      <Text>Add Event</Text>
-    </>
-  );
+interface FormValues {
+  eventName: string;
+  description: string;
+  date: string;
 }
+
+const formSchema = yup.object({
+  eventName: yup.string().required().min(4),
+  description: yup.string().required().min(4),
+  date: yup.string().required(),
+});
+
+const buttonPressedStyle = {
+  backgroundColor: "#F57C01",
+  borderColor: "#F57C01",
+};
+
+const AddEvent: React.FC = () => {
+  const [isButtonPressed, setButtonPressed] = useState(false);
+  const [isSubmitted, setSubmitted] = useState(false);
+  const navigation = useNavigation();
+
+  const handleSubmit = async (
+    values: FormValues,
+    { resetForm }: { resetForm: () => void }
+  ) => {
+    try {
+      const docData = {
+        eventName: values.eventName,
+        description: values.description,
+        date: values.date,
+      };
+
+      await addDoc(collection(db, "calendarEvent"), docData);
+      resetForm();
+      setSubmitted(true);
+      showAlert();
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
+  };
+
+  const showAlert = () => {
+    Alert.alert("Event Added", "The event has been added successfully.", [
+      { text: "OK", onPress: () => navigation.goBack() },
+    ]);
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <Text style={styles.heading}>Add Event</Text>
+        <Formik
+          initialValues={{ eventName: "", description: "", date: "" }}
+          validationSchema={formSchema}
+          onSubmit={handleSubmit}
+        >
+          {(props: FormikProps<FormValues>) => (
+            <View style={styles.form}>
+              <Text style={styles.text}>Event Name:</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Enter event name..."
+                onChangeText={props.handleChange("eventName")}
+                value={props.values.eventName}
+                onBlur={props.handleBlur("eventName")}
+              />
+              <Text style={styles.errorText}>
+                {props.touched.eventName && props.errors.eventName}
+              </Text>
+              <Text style={styles.text}>Description:</Text>
+              <TextInput
+                multiline
+                minHeight={70}
+                style={styles.input}
+                placeholder="Enter event description..."
+                onChangeText={props.handleChange("description")}
+                value={props.values.description}
+                onBlur={props.handleBlur("description")}
+              />
+              <Text style={styles.errorText}>
+                {props.touched.description && props.errors.description}
+              </Text>
+              <Text style={styles.text}>Date:</Text>
+              <TextInput
+                style={styles.input}
+                placeholder="Enter event date..."
+                onChangeText={props.handleChange("date")}
+                value={props.values.date}
+                onBlur={props.handleBlur("date")}
+              />
+              <Text style={styles.errorText}>
+                {props.touched.date && props.errors.date}
+              </Text>
+              <TouchableOpacity
+                title="Add Event"
+                onPress={props.handleSubmit}
+                style={[
+                  styles.button,
+                  isButtonPressed ? buttonPressedStyle : null,
+                ]}
+                onPressIn={() => setButtonPressed(true)}
+                onPressOut={() => setButtonPressed(false)}
+                activeOpacity={1}
+              >
+                <Text style={styles.buttonText}>Add Event</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        </Formik>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  heading: {
+    textAlign: "center",
+    fontWeight: "bold",
+    margin: 20,
+    fontSize: 20,
+  },
+  form: {
+    alignSelf: "stretch",
+    margin: 10,
+  },
+  text: {
+    marginLeft: 10,
+    marginBottom: 10,
+  },
+  input: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: "#1B73E7",
+    padding: 10,
+    fontSize: 14,
+    borderRadius: 6,
+    marginLeft: 15,
+    marginRight: 15,
+    backgroundColor: "white",
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    backgroundColor: "#1B73E7",
+    padding: 10,
+    fontSize: 14,
+    borderRadius: 6,
+    margin: 15,
+    borderColor: "#1B73E7",
+  },
+  buttonText: {
+    color: "white",
+  },
+  errorText: {
+    color: "crimson",
+    fontWeight: "bold",
+    marginBottom: 10,
+    textAlign: "center",
+    fontSize: 11,
+  },
+});
+
+export default AddEvent;
