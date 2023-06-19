c3749581f036f6e943bbc62519a6078689e77b91
Vera Ferreira <verammferreira@gmail.com>
2023-06-19 10:22:16 +0100

    fix margins and padding

--- Screens/ManagementAnnouncements.tsx
+++ Screens/ManagementAnnouncements.tsx
@@ -1,4 +1,11 @@
-import { Text, View, StyleSheet, Button, Image, ScrollView } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  Image,
+  ScrollView,
+} from "react-native";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { db } from "../config/firebase";
 import { useEffect, useState } from "react";
@@ -8,13 +15,16 @@ export default function ManagementAnnouncements({ navigation }: any) {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const q = query(collection(db, "postAdminAnnouncement"), orderBy("timestamp"));
+    const q = query(
+      collection(db, "postAdminAnnouncement"),
+      orderBy("timestamp")
+    );
 
     const announcementQuery = onSnapshot(q, (querySnapshot) => {
       let announcementArr: Array<Object> = [];
 
       querySnapshot.forEach((doc) => {
-        announcementArr.push({...doc.data(), id: doc.id});
+        announcementArr.push({ ...doc.data(), id: doc.id });
       });
       setAnnouncementList(announcementArr);
       setIsLoading(false);
@@ -25,28 +35,32 @@ export default function ManagementAnnouncements({ navigation }: any) {
 
   return (
     <ScrollView>
-    <View>
-      <Text style={styles.heading}> Management Announcements</Text>
-    {isLoading ? (
-    <Text>Loading...</Text>
-  ) : (
-    announcementList.map((announcement: any, index) => {
-      return (
-        <View key={announcement.id} style={styles.postBody}>
-          <Text style={styles.title}>{announcement.title}</Text>
-          <Image source={{ uri:announcement.img}} style={styles.postImg} />
-          <Text style={styles.description}>{announcement.description}</Text>
-        </View>
-      )
-    })
-  )}
-  </View>
-  </ScrollView>
-  )
+      <View>
+        <Text style={styles.heading}> Management Announcements</Text>
+        {isLoading ? (
+          <Text>Loading...</Text>
+        ) : (
+          announcementList.map((announcement: any, index) => {
+            return (
+              <View key={announcement.id} style={styles.postBody}>
+                <Text style={styles.title}>{announcement.title}</Text>
+                <Image
+                  source={{ uri: announcement.img }}
+                  style={styles.postImg}
+                />
+                <Text style={styles.description}>
+                  {announcement.description}
+                </Text>
+              </View>
+            );
+          })
+        )}
+      </View>
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
-  
   heading: {
     fontFamily: "Poppins_700Bold",
     textAlign: "center",
@@ -72,10 +86,13 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     borderWidth: 2,
     borderColor: "#F4C01D",
-    alignSelf: 'center'
+    alignSelf: "center",
   },
   description: {
-    marginLeft: 15,
+    marginLeft: 25,
+    marginRight: 25,
+    paddingLeft: 15,
+    paddingRight: 15,
     marginBottom: 10,
     fontFamily: "Poppins_400Regular",
     textAlign: "center",
