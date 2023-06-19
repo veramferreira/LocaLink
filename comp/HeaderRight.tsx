1b37623432e9ae4c44e7506cfcc83bf390ae6f54
Alina Khvishchuk <alina.khvishchuk@gmail.com>
2023-06-19 19:52:15 +0100

    Profile setup root

--- comp/HeaderRight.tsx
+++ comp/HeaderRight.tsx
@@ -3,19 +3,24 @@ import { Text, View, StyleSheet, Button, Image } from "react-native";
 import colours from "../constants/colours";
 import { MyContext } from "../Context";
 import { FontAwesome5 } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
 
 export default function HeaderRight() {
   const { userContext } = useContext(MyContext);
+  const navigation = useNavigation();
 
   const userName = userContext?.userName || "";
 
   if (!userName) return null;
 
   return (
-    <View style={styles.headerRight}>
-      <Text style={styles.headerRightText}>{userName}</Text>
-      <FontAwesome5 name="user" size={20} color="white" />
-    </View>
+    <TouchableOpacity onPress={() => navigation.navigate("ProfileSetup")}>
+      <View style={styles.headerRight}>
+        <Text style={styles.headerRightText}>{userName}</Text>
+        <FontAwesome5 name="user" size={20} color="white" />
+      </View>
+    </TouchableOpacity>
   );
 }
 
