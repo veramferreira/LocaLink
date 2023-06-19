8214c3b0bc83e232452e0c2da7ad8e5c67b2dd7a
Vera Ferreira <verammferreira@gmail.com>
2023-06-19 10:17:31 +0100

    add styling to page

--- Screens/ManagementAnnouncements.tsx
+++ Screens/ManagementAnnouncements.tsx
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet, Button } from "react-native";
+import { Text, View, StyleSheet, Button, Image, ScrollView } from "react-native";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { db } from "../config/firebase";
 import { useEffect, useState } from "react";
@@ -23,17 +23,62 @@ export default function ManagementAnnouncements({ navigation }: any) {
     return () => announcementQuery();
   }, []);
 
-  return isLoading ? (
+  return (
+    <ScrollView>
+    <View>
+      <Text style={styles.heading}> Management Announcements</Text>
+    {isLoading ? (
     <Text>Loading...</Text>
   ) : (
     announcementList.map((announcement: any, index) => {
       return (
-        <View>
-          <Text>{announcement.title}</Text>
-          <Text>{announcement.img}</Text>
-          <Text>{announcement.description}</Text>
+        <View key={announcement.id} style={styles.postBody}>
+          <Text style={styles.title}>{announcement.title}</Text>
+          <Image source={{ uri:announcement.img}} style={styles.postImg} />
+          <Text style={styles.description}>{announcement.description}</Text>
         </View>
       )
     })
+  )}
+  </View>
+  </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  
+  heading: {
+    fontFamily: "Poppins_700Bold",
+    textAlign: "center",
+    fontWeight: "bold",
+    margin: 20,
+    fontSize: 20,
+  },
+  postBody: {
+    display: "flex",
+    alignContent: "center",
+  },
+  title: {
+    marginLeft: 15,
+    marginBottom: 10,
+    fontFamily: "Poppins_500Medium",
+    textAlign: "center",
+    fontSize: 18,
+    marginTop: 20,
+  },
+  postImg: {
+    width: 300,
+    height: 200,
+    borderRadius: 8,
+    borderWidth: 2,
+    borderColor: "#F4C01D",
+    alignSelf: 'center'
+  },
+  description: {
+    marginLeft: 15,
+    marginBottom: 10,
+    fontFamily: "Poppins_400Regular",
+    textAlign: "center",
+    marginTop: 10,
+  },
+});
