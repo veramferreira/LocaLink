44eb07577c03914767df3dcb10a119b4c3bccc07
Vera Ferreira <verammferreira@gmail.com>
2023-06-20 16:03:31 +0100

    fix layout admins role

--- Screens/AssignAdminsPage.tsx
+++ Screens/AssignAdminsPage.tsx
@@ -39,28 +39,34 @@ export default function AssignAdmins() {
   }, [CommunityUserList]);
 
   return (
-    <View>
+    <View style={styles.container}>
+      <Text style={styles.heading}>Community Roles</Text>
       {CommunityUserList.map((user) => {
         return (
-          <View>
-            <Text>Username: {user.userName}</Text>
-            <Text>Current Role: {user.role}</Text>
+          <View style={styles.usersWrapper}>
 
+            <Text style={styles.boldText}>👤 Username: <Text style={styles.normalText}>{user.userName}</Text></Text>
+            <Text style={styles.boldText}>🗂️ Current Role: <Text style={styles.normalText}>{user.role}</Text></Text>
+         
             {user.role !== "owner" ? (
-              <View>
+              <View style={styles.btnWrapper}>
                 <TouchableOpacity
                   onPress={() => {
                     AddRoleToUser(user.email, "admin");
                   }}
                 >
-                  <Text>Assign Admin</Text>
+                  <View style={styles.buttonAdd}>
+                  <Text style={styles.buttonText}>Assign Admin</Text>
+                  </View>
                 </TouchableOpacity>
                 <TouchableOpacity
                   onPress={() => {
                     AddRoleToUser(user.email, "resident");
                   }}
                 >
-                  <Text>Remove Admin</Text>
+                  <View style={styles.buttonRemove}>
+                  <Text style={styles.buttonText}>Remove Admin</Text>
+                  </View>
                 </TouchableOpacity>
               </View>
             ) : null}
@@ -71,4 +77,70 @@ export default function AssignAdmins() {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+container: {
+  display: "flex",
+},
+heading: {
+  fontFamily: "Poppins_700Bold",
+  textAlign: "center",
+  fontWeight: "bold",
+  margin: 20,
+  fontSize: 20,
+},
+usersWrapper: {
+  display: "flex",
+  padding: 10,
+  backgroundColor: "white",
+  margin: 10,
+  borderRadius: 8,
+},
+boldText: {
+  fontFamily: "Poppins_500Medium",
+  textAlign: "center",
+  padding: 5,
+  paddingTop: 10,
+},
+normalText: {
+  fontFamily: 'Poppins_400Regular',
+  textAlign: "center",
+},
+btnWrapper: {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center"
+},
+buttonAdd: {
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: "#1B73E7",
+  padding: 10,
+  fontSize: 14,
+  borderRadius: 6,
+  margin: 15,
+  borderColor: "#1B73E7",
+  shadowColor: '#171717',
+  shadowOffset: {width: -2, height: 2},
+  shadowOpacity: 0.4,
+  shadowRadius: 2,
+  color: "white",
+},
+buttonRemove: {
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: "crimson",
+  padding: 10,
+  fontSize: 14,
+  borderRadius: 6,
+  margin: 15,
+  shadowColor: '#171717',
+  shadowOffset: {width: -2, height: 2},
+  shadowOpacity: 0.4,
+  shadowRadius: 2,
+  color: "white"
+},
+buttonText: {
+  color: "white",
+  fontFamily: "Poppins_500Medium",
+},
+});
