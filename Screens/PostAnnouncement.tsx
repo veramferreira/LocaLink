0c6f4edb4a004994e2c79b6ec3447b0de7f11838
Alina Khvishchuk <alina.khvishchuk@gmail.com>
2023-06-22 11:04:15 +0100

    Add upload photo to Post Announcement

--- Screens/PostAnnouncement.tsx
+++ Screens/PostAnnouncement.tsx
@@ -7,15 +7,20 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   Alert,
-  ScrollView
+  ScrollView,
+  Image,
+  ActivityIndicator,
 } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 import React, { useContext, useState } from "react";
 import { Formik, FormikProps } from "formik";
-import { db } from "../config/firebase";
+import { db, storage } from "../config/firebase";
 import { addDoc, collection, serverTimestamp } from "@firebase/firestore";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import * as yup from "yup";
 import { useNavigation } from "@react-navigation/native";
 import { MyContext } from "../Context";
+import colours from "../constants/colours";
 
 interface FormValues {
   title: string;
@@ -36,10 +41,62 @@ const buttonPressedStyle = {
 
 export default function PostAnnouncement() {
   const [isButtonPressed, setButtonPressed] = useState(false);
+  const [currentImage, setCurrentImage] = useState(null);
+  const [uploading, setUploading] = useState(false);
+  const [downloadUrl, setDownloadUrl] = useState("");
 
   const { userContext } = useContext(MyContext);
 
   const navigation = useNavigation();
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    if (!result.canceled) {
+      const source = { uri: result.assets[0].uri };
+
+      setCurrentImage(source);
+      handleUploadImage(source);
+    }
+  };
+
+  const handleUploadImage = async (image) => {
+    if (!image) return;
+
+    setUploading(true);
+    const response = await fetch(image.uri);
+    const blob = await response.blob();
+    const filename = image.uri.substring(image.uri.lastIndexOf("/") + 1);
+    const storageRef = ref(storage, filename);
+    const uploadTask = uploadBytesResumable(storageRef, blob);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        console.log(`Upload is ${progress}% complete`);
+      },
+      (error) => {
+        console.error("Error uploading image:", error);
+        setUploading(false);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
+          console.log("File available at", downloadUrl);
+
+          setDownloadUrl(downloadUrl);
+          setUploading(false);
+          Alert.alert("Photo uploaded");
+        });
+      }
+    );
+  };
 
   const handleSubmit = async (
     values: FormValues,
@@ -49,7 +106,7 @@ export default function PostAnnouncement() {
       const docData = {
         title: values.title,
         description: values.description,
-        img: values.img,
+        img: downloadUrl,
         timestamp: serverTimestamp(),
       };
 
@@ -60,6 +117,9 @@ export default function PostAnnouncement() {
 
       await addDoc(collectionRef, docData);
       resetForm();
+      setCurrentImage(null);
+
+      setDownloadUrl("");
       showAlert();
     } catch (error) {
       console.error("Error adding document: ", error);
@@ -93,59 +153,84 @@ export default function PostAnnouncement() {
           onSubmit={handleSubmit}
         >
           {(props: FormikProps<FormValues>) => (
-            <ScrollView scrollIndicatorInsets={{right: 1}}>
-            <View style={styles.form}>
-              <Text style={styles.text}>Title: </Text>
-              <TextInput
-                style={styles.input}
-                placeholder="announcement title..."
-                onChangeText={(text) => props.handleChange("title")(text)}
-                value={props.values.title}
-                onBlur={props.handleBlur("title")}
-              />
-              <Text style={styles.errorText}>
-                {props.touched.title && props.errors.title}
-              </Text>
-              <Text style={styles.text}>Announcement: </Text>
-              <TextInput
-                multiline
-                minHeight={70}
-                style={styles.input}
-                placeholder="describe the announcement... "
-                onChangeText={(text) => props.handleChange("description")(text)}
-                value={props.values.description}
-                onBlur={props.handleBlur("description")}
-              />
-              <Text style={styles.errorText}>
-                {props.touched.description && props.errors.description}
-              </Text>
-              <Text style={styles.text}>
-                Image URL <Text style={styles.optionalText}>(optional)</Text>:
-              </Text>
-              <TextInput
-                style={styles.input}
-                placeholder="Image URL..."
-                onChangeText={(text) => props.handleChange("img")(text)}
-                value={props.values.img}
-                onBlur={props.handleBlur("img")}
-              />
-              <Text style={styles.errorText}>
-                {props.touched.img && props.errors.img}
-              </Text>
-              <TouchableOpacity
-                title="submit!"
-                onPress={props.handleSubmit}
-                style={[
-                  styles.button,
-                  isButtonPressed ? buttonPressedStyle : null,
-                ]}
-                onPressIn={() => setButtonPressed(true)}
-                onPressOut={() => setButtonPressed(false)}
-                activeOpacity={1}
-              >
-                <Text style={styles.buttonText}>Post!</Text>
-              </TouchableOpacity>
-            </View>
+            <ScrollView scrollIndicatorInsets={{ right: 1 }}>
+              <View style={styles.form}>
+                <Text style={styles.text}>Title: </Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="announcement title..."
+                  onChangeText={(text) => props.handleChange("title")(text)}
+                  value={props.values.title}
+                  onBlur={props.handleBlur("title")}
+                />
+                <Text style={styles.errorText}>
+                  {props.touched.title && props.errors.title}
+                </Text>
+                <Text style={styles.text}>Announcement: </Text>
+                <TextInput
+                  multiline
+                  minHeight={70}
+                  style={styles.input}
+                  placeholder="describe the announcement... "
+                  onChangeText={(text) =>
+                    props.handleChange("description")(text)
+                  }
+                  value={props.values.description}
+                  onBlur={props.handleBlur("description")}
+                />
+                <Text style={styles.errorText}>
+                  {props.touched.description && props.errors.description}
+                </Text>
+                <Text style={styles.text}>
+                  Image URL <Text style={styles.optionalText}>(optional)</Text>:
+                </Text>
+                <View style={styles.buttonsWrapper}>
+                  {!currentImage && (
+                    <TouchableOpacity
+                      onPress={pickImage}
+                      style={styles.pickImage}
+                    >
+                      <MaterialCommunityIcons
+                        name="file-upload-outline"
+                        size={24}
+                        color={colours.font}
+                        style={styles.buttonIcon}
+                      />
+                      <Text style={styles.buttonTextUpload}>Pick Image</Text>
+                    </TouchableOpacity>
+                  )}
+                </View>
+                <View style={styles.imageViewWrapper}>
+                  <View style={styles.imageViewUploadWrapper}>
+                    {currentImage && (
+                      <Image
+                        source={currentImage}
+                        style={styles.selectedImage}
+                      />
+                    )}
+                    {uploading && (
+                      <ActivityIndicator
+                        size="large"
+                        color={colours.secondary}
+                        style={styles.loading}
+                      />
+                    )}
+                  </View>
+                </View>
+                <TouchableOpacity
+                  title="submit!"
+                  onPress={props.handleSubmit}
+                  style={[
+                    styles.button,
+                    isButtonPressed ? buttonPressedStyle : null,
+                  ]}
+                  onPressIn={() => setButtonPressed(true)}
+                  onPressOut={() => setButtonPressed(false)}
+                  activeOpacity={1}
+                >
+                  <Text style={styles.buttonText}>Post!</Text>
+                </TouchableOpacity>
+              </View>
             </ScrollView>
           )}
         </Formik>
@@ -227,5 +312,60 @@ const styles = StyleSheet.create({
   textSubmitted: {
     textAlign: "center",
     margin: 20,
+  },
+  selectedImage: {
+    width: 100,
+    height: 100,
+    resizeMode: "contain",
+  },
+  pickImage: {
+    width: "100%",
+    height: 40,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingLeft: 8,
+    borderStyle: "dashed",
+    borderColor: colours.font,
+    borderWidth: 1,
+    borderRadius: 4,
+  },
+  pickImageView: {
+    width: 200,
+    height: 100,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingLeft: 8,
+    borderColor: colours.font,
+    borderRadius: 4,
+    marginLeft: 8,
+  },
+  buttonIcon: {
+    marginRight: 4,
+  },
+  buttonTextUpload: {
+    color: colours.font,
+    fontFamily: "Poppins_400Regular",
+  },
+  buttonsWrapper: {
+    width: "100%",
+    alignItems: "center",
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+  imageViewWrapper: {
+    flexDirection: "row",
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+  imageViewUploadWrapper: {
+    position: "relative",
+  },
+  loading: {
+    position: "absolute",
+    left: 50,
+    top: 50,
+    transform: [{ translateX: -16 }, { translateY: -16 }],
   },
 });
